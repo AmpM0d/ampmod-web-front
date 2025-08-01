577554570542aec11aca8114762212744b559bdb
AmpElectrecuted <aec@noreply.codeberg.org>
2025-08-01 23:23:40 +0100

    some precautions to prevent spam

--- netlify/edge-functions/bytedance.js
+++ netlify/edge-functions/bytedance.js
@@ -1,11 +1,11 @@
-const BLOCKED_USER_AGENTS = ['Bytespider', 'ByteDance'];
+const BLOCKED_USER_AGENTS = ['Bytespider', 'Bytedance'];
 
 export default async (request) => {
   const userAgent = request.headers.get('user-agent');
   
   if (userAgent) {
     const lowerCaseUserAgent = userAgent.toLowerCase();
-    
+  
     if (BLOCKED_USER_AGENTS.some(agent => lowerCaseUserAgent.includes(agent.toLowerCase()))) {
       console.log(`Disguising access from user agent: ${userAgent} with a 404.`);
       
