632bb9a296a7a017a29b7f6d2f8e27180632af46
AmpElectrecuted <aec@noreply.codeberg.org>
2025-08-01 20:45:24 +0100

    add netlifly adapater, idk if we will use this for long but still

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
- import { fileURLToPath } from 'url';
- import { join, dirname } from 'path';
- const __filename = fileURLToPath(import.meta.url);
- const __dirname = dirname(__filename);
-  const path_to_layout = join(__dirname, './src/mdsvex.svelte');
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import adapter from '@sveltejs/adapter-netlify';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const path_to_layout = join(__dirname, './src/mdsvex.svelte');
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex({layout: path_to_layout})],
